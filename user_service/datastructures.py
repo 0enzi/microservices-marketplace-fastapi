7d931afbdc1e15fcb85c22c26b5cf612ad21b522
Elvis Chege <elvischege@icloud.com>
2023-01-01 15:27:03 +0200

    working on user_service

--- user_service/datastructures.py
+++ user_service/datastructures.py
@@ -1,4 +1,12 @@
-from pydantic import BaseModel
+
+from pydantic import BaseModel, EmbeddedModel
+
+
+class AccountType(BaseModel):
+    account_type_name:str
+    permissions: list
+    open_for_registration: bool
+    status: bool
 
 
 class UsernamePasswordForm(BaseModel):
@@ -10,6 +18,8 @@ class UserForm(UsernamePasswordForm):
     email: str = None
     full_name: str = None
     user_type: str
+    location: str
+    account_type: EmbeddedModel[AccountType]
 
 
 class UserUpdateForm(BaseModel):
@@ -17,13 +27,19 @@ class UserUpdateForm(BaseModel):
     email: str = None
     full_name: str = None
     user_type: str = None
+    location: str = None
 
 
 class UserInDb(BaseModel):
-    id: int
+    full_name: str = None
     username: str
     email: str = None
-    full_name: str = None
-    user_type: str
     hashed_password: str
-    created_by: int
+    phone: str = None
+    location: str = None
+    created_at: str
+    account_type: EmbeddedModel[AccountType]
+    is_super_admin: bool
+    status: bool
+    email_verified: bool
+    phone_verified: bool
---
+++ user_service/readme.md
@@ -0,0 +1,17 @@
+# Users Microservice
+
+**This microservice handles the creation and management of user accounts, it will be used by admin for performing user roles changes. It **
+
+## Tech Stack
+
+| Core Libraries    | Database |
+| ----------------- | :------: |
+| PyMongo, ODMantic | MongoDB  |
+
+### ODMantic
+
+Sync and Async ODM (Object Document Mapper) for MongoDB based on standard python type hints. Built on top of pydantic for model definition and validation.
+
+### Models
+
+User Model Attributes as followed:
