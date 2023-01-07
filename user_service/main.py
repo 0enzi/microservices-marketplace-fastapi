4e7fd5ff5d7ef3fa7d51fb0f4691c2d40209af58
Elvis Chege <elvischege@icloud.com>
2023-01-07 05:56:30 +0200

    Update routes

--- user_service/main.py
+++ user_service/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, APIRouter, Body, Request, Response, HTTPException, status
 from dotenv import dotenv_values
 from pymongo import MongoClient
-from .api.v1.users import user_routes
+from api.v1 import api_router as user_routes
 
 
 import datetime
 from models import UserInDB as User
 from models import UsernamePasswordForm, UserForm, UserUpdate
-from auth import verify_password, get_password_hash
+# from usauth import verify_password, get_password_hash
 config = dotenv_values(".env")
 
 app = FastAPI()
-app.include_router(user_routes, prefix=settings.API_V1_STR)
+app.include_router(user_routes, prefix="settings.API_V1_STR")
 
 # Connect to MongoDB
 client = MongoClient("mongodb://localhost:27017/")
