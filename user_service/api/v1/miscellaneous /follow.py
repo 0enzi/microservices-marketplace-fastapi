af47a2d59f7b5b6313baf7a16da621450435124b
Elvis Chege <elvischege@icloud.com>
2023-01-13 16:41:32 +0200

    Added following user

---
+++ user_service/api/v1/miscellaneous /follow.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, status, Response, HTTPException, Request
+import json
+
+from fastapi import APIRouter, Body, Request, Response, HTTPException, status
+from bson.json_util import dumps, loads
+
+import datetime
+from models import UserInDB as User
+from models import UsernamePasswordForm, UserForm, UserUpdate
+from models import UserResponse
+from .auth import verify_password, get_password_hash
+router = APIRouter()
+
+CACHE_KEY_PREFIX = "user:"
+
+
+@router.put("/follow/@{username}")
+def follow_user(request: Request, username: str):
+
+    # user_id = request.app.current_user
+    user_id = "5f9f1b0b0b9b9b0b9b0b9b0b" # current user id provided by jwt
+
+    result = request.app.database["users"].update_many({"_id": user_id}, {"$push": {"following_users": username}}) # add current user to following_users list of user with username
+    if result.modified_count == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+    cache_key = CACHE_KEY_PREFIX + user_id
+    request.app.redis_client.set(cache_key, json.dumps(user_id))
