e9d406061c6d4aaf811db341d5b1622b2596f75a
Elvis Chege <elvischege@icloud.com>
2023-01-14 08:17:30 +0200

    Error

--- listing_service/api/v1/miscellaneous /follow.py
+++ listing_service/api/v1/miscellaneous /follow.py
@@ -10,21 +10,21 @@ import datetime
 router = APIRouter()
 
 
-@router.put("/follow/{listing_id}")
-def follow_user(request: Request, listing_id: str):
+@router.put("/follow/{category_id}")
+def follow_user(request: Request, category_id: str):
 
     # user_id = request.app.current_user
     user_id = "5f9f1b0b0b9b9b0b9b0b9b0b" # current user id provided by jwt
 
-    result = request.app.database["users"].update_many({"_id": user_id}, {"$push": {"following_listings": listing_id}}) 
+    result = request.app.database["users"].update_many({"_id": user_id}, {"$push": {"following_categories": category_id}}) 
     return result
 
 
-@router.put("/unfollow/{listing_id}")
-def follow_user(request: Request, listing_id: str):
+@router.put("/unfollow/{category_id}")
+def follow_user(request: Request, category_id: str):
      # user_id = request.app.current_user
     user_id = "5f9f1b0b0b9b9b0b9b0b9b0b" # current user id provided by jwt
 
-    result = request.app.database["users"].update_many({"_id": user_id}, {"$pull": {"following_listings": listing_id}})
+    result = request.app.database["users"].update_many({"_id": user_id}, {"$pull": {"following_categories": category_id}})
     return result
 
