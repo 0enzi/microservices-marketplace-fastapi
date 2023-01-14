955a8f43bf704ee82caf56072f27d3c3b9ea671e
Elvis Chege <elvischege@icloud.com>
2023-01-14 07:20:54 +0200

    Update docker microservices and debugging & testing

--- user_service/api/v1/miscellaneous /follow.py
+++ user_service/api/v1/miscellaneous /follow.py
@@ -5,10 +5,8 @@ from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from bson.json_util import dumps, loads
 
 import datetime
-from models import UserInDB as User
-from models import UsernamePasswordForm, UserForm, UserUpdate
-from models import UserResponse
-from .auth import verify_password, get_password_hash
+
+
 router = APIRouter()
 
 CACHE_KEY_PREFIX = "user:"
@@ -18,6 +16,16 @@ CACHE_KEY_PREFIX = "user:"
 def follow_user(request: Request, username: str):
 
     # user_id = request.app.current_user
+    user_id = "5f9f1b0b0b9b9b0b9b0b9b0b" # current user id provided by jwt
+
+    result = request.app.database["users"].update_many({"_id": user_id}, {"$push": {"following_users": username}}) # add current user to following_users list of user with username
+    if result.modified_count == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+@router.put("/unfollow/@{username}")
+def follow_user(request: Request, username: str):
+     # user_id = request.app.current_user
     user_id = "5f9f1b0b0b9b9b0b9b0b9b0b" # current user id provided by jwt
 
     result = request.app.database["users"].update_many({"_id": user_id}, {"$push": {"following_users": username}}) # add current user to following_users list of user with username
